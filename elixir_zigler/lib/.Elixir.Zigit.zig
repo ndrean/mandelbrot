7208ddf5af078118d6fd64e8853e3b4280ccf761
Neven DREAN <nevendrean@yahoo.fr>
2024-11-07 22:49:09 +0100

    ok zigler livebook & concurrent nx

--- elixir_zigler/lib/.Elixir.Zigit.zig
+++ elixir_zigler/lib/.Elixir.Zigit.zig
@@ -2,43 +2,52 @@ const beam = @import("beam");
 const std = @import("std");
 const Cx = std.math.Complex(f64);
 
+const print = std.debug.print;
+
 const IMAX = 100;
-const topLeft = Cx{ .re = -2.0, .im = 1.2 };
+const topLeft = Cx{ .re = -2.1, .im = 1.2 };
 const bottomRight = Cx{ .re = 0.6, .im = -1.2 };
 const w = bottomRight.re - topLeft.re;
-const h = topLeft.im - bottomRight.im;
+const h = bottomRight.im - topLeft.im;
+
+const Context = struct {
+    res_x: usize,
+    res_y: usize,
+};
 
 /// nif: generate_mandelbrot/2 Threaded
-pub fn generate_mandelbrot(res_x: usize, res_y: usize, sym: bool) !beam.term {
+pub fn generate_mandelbrot(res_x: usize, res_y: usize) !beam.term {
     const pixels = try beam.allocator.alloc(u8, res_x * res_y * 3);
     defer beam.allocator.free(pixels);
 
-    // const res = try createUnthreadedSlice(pixels, res_x, res_y);
-    const res = try createBands(pixels, res_x, res_y, sym);
+    // const res = createUnthreadedSlice(pixels, res_x, res_y);
+    // threaded version
+    const resolution = Context{ .res_x = res_x, .res_y = res_y };
+    const res = try createBands(pixels, resolution);
     return beam.make(res, .{ .as = .binary });
     // return beam.make(res, .{});
 }
 
-fn createBands(pixels: []u8, res_x: usize, res_y: usize, sym: bool) ![]u8 {
-    // const pixels = try allocator.alloc(u8, ctx.resolution[0] * ctx.resolution[1] * 3);
-    // errdefer allocator.free(pixels);
-
+// <--- threaded version
+fn createBands(pixels: []u8, ctx: Context) ![]u8 {
     const cpus = try std.Thread.getCpuCount();
     var threads = try beam.allocator.alloc(std.Thread, cpus);
     defer beam.allocator.free(threads);
 
-    // half of the total rows if sym true
-    var rows_to_process: usize = res_x;
-    if (sym) rows_to_process = res_x / 2 + res_x % 2;
-
+    // half of the total rows
+    const rows_to_process = ctx.res_y / 2 + ctx.res_y % 2;
     // one band is one count of cpus
-    const nb_rows_per_band = rows_to_process / cpus + rows_to_process % cpus;
-    std.debug.print("nb_rows_per_band: {}\n", .{nb_rows_per_band});
+    // const nb_rows_per_band = rows_to_process / cpus + rows_to_process % cpus;
+    const rows_per_band = (rows_to_process + cpus - 1) / cpus;
 
     for (0..cpus) |cpu_count| {
-        const start_row = cpu_count * nb_rows_per_band;
-        const end_row = start_row + nb_rows_per_band;
-        const args = .{ res_x, res_y, pixels, start_row, end_row };
+        const start_row = cpu_count * rows_per_band;
+
+        // Stop if there are no rows to process
+        if (start_row >= rows_to_process) break;
+
+        const end_row = @min(start_row + rows_per_band, rows_to_process);
+        const args = .{ ctx, pixels, start_row, end_row };
         threads[cpu_count] = try std.Thread.spawn(.{}, processRows, args);
     }
     for (threads[0..cpus]) |thread| {
@@ -48,25 +57,31 @@ fn createBands(pixels: []u8, res_x: usize, res_y: usize, sym: bool) ![]u8 {
     return pixels;
 }
 
-fn processRow(res_x: usize, res_y: usize, pixels: []u8, row_id: usize) void {
+fn processRows(ctx: Context, pixels: []u8, start_row: usize, end_row: usize) void {
+    for (start_row..end_row) |current_row| {
+        processRow(ctx, pixels, current_row);
+    }
+}
+
+fn processRow(ctx: Context, pixels: []u8, row_id: usize) void {
     // Calculate the symmetric row
-    const sym_row_id = res_x - 1 - row_id;
+    const sym_row_id = ctx.res_y - 1 - row_id;
 
     if (row_id <= sym_row_id) {
         // loop over columns
-        for (0..res_y) |col_id| {
-            const c = pixelToComplex(.{ @as(usize, @intCast(col_id)), @as(usize, @intCast(row_id)) }, res_x, res_y);
-            const iter = getIter(c);
+        for (0..ctx.res_x) |col_id| {
+            const c = mapPixel(.{ @as(usize, @intCast(row_id)), @as(usize, @intCast(col_id)) }, ctx);
+            const iter = iterationNumber(c);
             const colour = createRgb(iter);
 
-            const p_idx = (row_id * res_y + col_id) * 3;
+            const p_idx = (row_id * ctx.res_x + col_id) * 3;
             pixels[p_idx + 0] = colour[0];
             pixels[p_idx + 1] = colour[1];
             pixels[p_idx + 2] = colour[2];
 
             // Process the symmetric row (if it's different from current row)
             if (row_id != sym_row_id) {
-                const sym_p_idx = (sym_row_id * res_y + col_id) * 3;
+                const sym_p_idx = (sym_row_id * ctx.res_x + col_id) * 3;
                 pixels[sym_p_idx + 0] = colour[0];
                 pixels[sym_p_idx + 1] = colour[1];
                 pixels[sym_p_idx + 2] = colour[2];
@@ -75,47 +90,22 @@ fn processRow(res_x: usize, res_y: usize, pixels: []u8, row_id: usize) void {
     }
 }
 
-fn processRows(res_x: usize, res_y: usize, pixels: []u8, start_row: usize, end_row: usize) void {
-    for (start_row..end_row) |current_row| {
-        processRow(res_x, res_y, pixels, current_row);
-    }
+fn mapPixel(pixel: [2]usize, ctx: Context) Cx {
+    const px_width = ctx.res_x - 1;
+    const px_height = ctx.res_y - 1;
+    const scale_x = w / @as(f64, @floatFromInt(px_width));
+    const scale_y = h / @as(f64, @floatFromInt(px_height));
+
+    const re = topLeft.re + scale_x * @as(f64, @floatFromInt(pixel[1]));
+    const im = topLeft.im + scale_y * @as(f64, @floatFromInt(pixel[0]));
+    return Cx{ .re = re, .im = im };
 }
 
-fn createUnthreadedSlice(pixels: []u8, res_x: usize, res_y: usize) ![]u8 {
-    const rows_to_process = res_x / 2 + res_x % 2;
-    for (0..rows_to_process) |current_row| {
-        for (0..res_y) |current_col| {
-            const c = pixelToComplex(.{ @as(u32, @intCast(current_col)), @as(u32, @intCast(current_row)) }, res_x, res_y);
-            const iter = getIter(c);
-            const colour = createRgb(iter);
-            const pixel_index = (current_row * res_y + current_col) * 3;
-            // copy RGB values to consecutive memory locations
-            pixels[pixel_index + 0] = colour[0]; //R
-            pixels[pixel_index + 1] = colour[1]; //G
-            pixels[pixel_index + 2] = colour[2]; //B
-
-            const mirror_y = res_x - 1 - current_row;
-            if (mirror_y != current_row) {
-                const mirror_pixel_index = (mirror_y * res_y + current_col) * 3;
-                pixels[mirror_pixel_index + 0] = colour[0]; //R
-                pixels[mirror_pixel_index + 1] = colour[1]; //G
-                pixels[mirror_pixel_index + 2] = colour[2]; //B
-            }
-        }
-    }
-    //return beam.make(pixels, .{});
-    return pixels;
-}
-
-fn pixelToComplex(pix: [2]usize, res_x: usize, res_y: usize) Cx {
-    const re = @as(f64, @floatFromInt(pix[0])) / @as(f64, @floatFromInt(res_x)) * w;
-    const im = @as(f64, @floatFromInt(pix[1])) / @as(f64, @floatFromInt(res_y)) * h;
-    return Cx{ .re = (topLeft.re + re) * w, .im = (topLeft.im - im) * h };
-}
-
-fn getIter(c: Cx) ?usize {
+fn iterationNumber(c: Cx) ?usize {
     if (c.re > 0.6 or c.re < -2.1) return null;
     if (c.im > 1.2 or c.im < -1.2) return null;
+    // first cardiod
+    if ((c.re + 1) * (c.re + 1) + c.im * c.im < 0.0625) return null;
 
     var z = Cx{ .re = 0.0, .im = 0.0 };
 
@@ -145,3 +135,28 @@ fn createRgb(iter: ?usize) [3]u8 {
         return [_]u8{ 0, @as(u8, @intFromFloat(127 * (1 - scaled))), @as(u8, @intFromFloat(255.0 * (1.0 - scaled))) };
     }
 }
+
+// fn createUnthreadedSlice(pixels: []u8, res_x: usize, res_y: usize) []u8 {
+//     const rows_to_process = res_x / 2 + res_x % 2;
+//     for (0..rows_to_process) |current_row| {
+//         for (0..res_y) |current_col| {
+//             const c = mapPixel(.{ @as(u64, @intCast(current_col)), @as(u64, @intCast(current_row)) }, res_x, res_y);
+//             const iter = iterationNumber(c);
+//             const colour = createRgb(iter);
+//             const pixel_index = (current_row * res_x + current_col) * 3;
+//             // copy RGB values to consecutive memory locations
+//             pixels[pixel_index + 0] = colour[0]; //R
+//             pixels[pixel_index + 1] = colour[1]; //G
+//             pixels[pixel_index + 2] = colour[2]; //B
+
+//             const mirror_y = res_y - 1 - current_row;
+//             if (mirror_y != current_row) {
+//                 const mirror_pixel_index = (mirror_y * res_y + current_col) * 3;
+//                 pixels[mirror_pixel_index + 0] = colour[0]; //R
+//                 pixels[mirror_pixel_index + 1] = colour[1]; //G
+//                 pixels[mirror_pixel_index + 2] = colour[2]; //B
+//             }
+//         }
+//     }
+// }
+//     return pixels;
--- elixir_zigler/lib/zigit.ex
+++ elixir_zigler/lib/zigit.ex
@@ -2,16 +2,18 @@ defmodule Zigit do
   use Zig,
     otp_app: :zigit,
     nifs: [..., generate_mandelbrot: [:threaded]],
-    zig_code_path: "draw.zig",
-    release_mode: :fast
+    zig_code_path: "draw.zig"
+
+  # release_mode: :fast
 
   def draw do
-    t = :os.system_time(:millisecond)
+    t = :os.system_time(:microsecond)
     res_x = 1_000
     res_y = 1_000
-    img = generate_mandelbrot(res_x, res_y, true)
-    # dbg(byte_size(img))
-    dbg(:os.system_time(:millisecond) - t)
+
+    img = generate_mandelbrot(res_x, res_y)
+    IO.puts(byte_size(img))
+    IO.puts(:os.system_time(:microsecond) - t)
     {:ok, vimg} = Vix.Vips.Image.new_from_binary(img, res_x, res_y, 3, :VIPS_FORMAT_UCHAR)
     Vix.Vips.Operation.pngsave(vimg, "priv/zigex.png", compression: 2)
     # Vix.Vips.Image.write_to_file(vimg, "priv/zigex.jpg[Q=90]")
